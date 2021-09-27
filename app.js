c031a759c114c57c063f818f26f168845af05be3
Benjamin Heffner <bdheffner1227@gmail.com>
2021-09-27 15:33:50 -0400

    Fixed issue where the Clear All button wasn't working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Selectors
 document.querySelector('form').addEventListener('submit', handleSubmitForm);
 document.querySelector('ul').addEventListener('click', handleClickDeleteOrCheck);
+document.getElementById('clearAll').addEventListener('click', handleClearAll)
 
 // Event Handlers
 function handleSubmitForm(e) {
